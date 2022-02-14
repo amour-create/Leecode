f2b50531e4a66903f7e3c69299ca6c6752b64d49
amour-create <1447826510@qq.com>
2022-02-14 16:48:04 +0800

    minDisDP

--- Dynamic/src/Solution.java
+++ Dynamic/src/Solution.java
@@ -190,7 +190,25 @@ public class Solution {
         return maxvalue;
     }
 
-
+    public int minDisDP(int[][] matrix, int n){
+        int[][] states = new int[n][n];
+        int sum = 0;
+        for (int j = 0; j < n; ++j) {
+            sum += matrix[0][j];
+            states[0][j] = sum;
+        }
+        sum = 0;
+        for(int i = 0; i < n; ++i){
+            sum += matrix[i][0];
+            states[i][0] = sum;
+        }
+        for(int i = 1; i < n; ++i){
+            for(int j = 1; j < n; ++j){
+                states[i][j] = matrix[i][j] + Math.min(states[i][j-1], states[i-1][j]);
+            }
+        }
+        return states[n-1][n-1];
+    }
 
     
     
